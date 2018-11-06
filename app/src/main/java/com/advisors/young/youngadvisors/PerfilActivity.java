04398d592409a88c36f0b2754e575aa23e9e8b70
X Squad <43920315+AguilasBuldingCode@users.noreply.github.com>
2018-11-06 00:10:23 -0600

    Carga de foto en registro v2

--- app/src/main/java/com/advisors/young/youngadvisors/PerfilActivity.java
+++ app/src/main/java/com/advisors/young/youngadvisors/PerfilActivity.java
@@ -2,10 +2,18 @@ package com.advisors.young.youngadvisors;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentHostCallback;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,13 +21,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class PerfilActivity extends AppCompatActivity {
 
     private static final int COD_SELECCIONADA = 10;
     private static final int COD_FOTO = 20;
+    private static final String CARPETA_IMAGEN = "imagenes";
+    private static final String CARPETA_PRINCIPAL = "misImagenesApp";
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;
+    private final int MIS_PERMISOS = 100;
+
+    File fileImagen;
+    Bitmap bitmap;
+    private String path;
+
     String  Nombre, Apellidos, Correo, Telefono, Usuario, Password, Respuesta1, Respuesta2, Pregunta1, Pregunta2;
     TextView bienvenido;
     ImageView Foto;
@@ -44,6 +66,33 @@ public class PerfilActivity extends AppCompatActivity {
         Foto = findViewById(R.id.IVPerfil);
     }
 
+    private void abriCamara() {
+        File miFile=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+        if(isCreada){
+            isCreada=miFile.mkdirs();
+        }
+        if(isCreada){
+            Long consecutivo= System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+            path=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN
+                    +File.separator+nombre;//indicamos la ruta de almacenamiento
+            fileImagen=new File(path);
+            Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(fileImagen));
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
+            {
+                String authorities=this.getPackageName()+".provider";
+                Uri imageUri= FileProvider.getUriForFile(this,authorities,fileImagen);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            }else
+            {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(fileImagen));
+            }
+            startActivityForResult(intent,COD_FOTO);
+        }
+    }
+
     public void subirFoto(View view){
         final CharSequence[] opciones={"Tomar Foto","Elegir de Galeria","Cancelar"};
         final AlertDialog.Builder builder=new AlertDialog.Builder(this);
@@ -52,7 +101,7 @@ public class PerfilActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 if (opciones[i].equals("Tomar Foto")){
-                    //abriCamara();
+                    abriCamara();
                 }else{
                     if (opciones[i].equals("Elegir de Galeria")){
                         Intent intent=new Intent(Intent.ACTION_PICK,
@@ -68,6 +117,25 @@ public class PerfilActivity extends AppCompatActivity {
         builder.show();
     }
 
+    private Bitmap redimensionarImagen(Bitmap bitmap, float anchoNuevo, float altoNuevo) {
+
+        int ancho=bitmap.getWidth();
+        int alto=bitmap.getHeight();
+
+        if(ancho>anchoNuevo || alto>altoNuevo){
+            float escalaAncho=anchoNuevo/ancho;
+            float escalaAlto= altoNuevo/alto;
+
+            Matrix matrix=new Matrix();
+            matrix.postScale(escalaAncho,escalaAlto);
+
+            return Bitmap.createBitmap(bitmap,0,0,ancho,alto,matrix,false);
+
+        }else{
+            return bitmap;
+        }
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -76,16 +144,15 @@ public class PerfilActivity extends AppCompatActivity {
             case COD_SELECCIONADA:
                 Uri miPath=data.getData();
                 Foto.setImageURI(miPath);
-                //try {
-                //   bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),miPath);
-                //    Foto.setImageBitmap(bitmap);
-                //} catch (IOException e) {
-                //    e.printStackTrace();
-                //}
+                try {
+                   bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),miPath);
+                    Foto.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case COD_FOTO:
-                /*
-                MediaScannerConnection.scanFile(getContext(), new String[]{path}, null,
+                MediaScannerConnection.scanFile(this, new String[]{path}, null,
                         new MediaScannerConnection.OnScanCompletedListener() {
                             @Override
                             public void onScanCompleted(String path, Uri uri) {
@@ -95,9 +162,37 @@ public class PerfilActivity extends AppCompatActivity {
 
                 bitmap= BitmapFactory.decodeFile(path);
                 Foto.setImageBitmap(bitmap);
-                */
                 break;
         }
-        //bitmap=redimensionarImagen(bitmap,600,800);
+        bitmap=redimensionarImagen(bitmap,600,800);
+    }
+
+    private void cargarDialogoRecomendacion() {
+        AlertDialog.Builder dialogo=new AlertDialog.Builder(this);
+        dialogo.setTitle("Permisos Desactivados");
+        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
+
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+            }
+        });
+        dialogo.show();
+    }
+
+    private boolean solicitaPermisosVersionesSuperiores() {
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.M){//validamos si estamos en android menor a 6 para no buscar los permisos
+            return true;
+        }
+        if((this.checkSelfPermission(WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)&&this.checkSelfPermission(CAMERA)==PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        if ((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)||(shouldShowRequestPermissionRationale(CAMERA)))){
+            cargarDialogoRecomendacion();
+        }else{
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MIS_PERMISOS);
+        }
+        return false;
     }
 }
